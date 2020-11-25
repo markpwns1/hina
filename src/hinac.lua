5daebd4675de97cc64bd557a48dcb58dffcc11f9
Mark <mark_kaldas@yahoo.com>
2020-11-24 17:14:24 -0700

    fix paths and includes

--- src/hinac.lua
+++ src/hinac.lua
@@ -1,5 +1,5 @@
 local __h_filename = arg[0]
-local __h_slash = string.find(__h_filename, "/[^/]*$") or 0
+local __h_slash = string.find(__h_filename, "/[^/]*$") or string.find(__h_filename, "\\[^\\]*$") or 0
 local __h_current_dir = string.sub(__h_filename, 1, __h_slash - 1)
 package.path = package.path .. ";" .. __h_current_dir .. "\\?.lua"
 package.cpath = package.cpath .. ";" .. __h_current_dir .. "\\?.dll"
